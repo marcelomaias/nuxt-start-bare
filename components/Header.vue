8c555613d22af5e2eb2957143d0128de45b4e56e
Maia <maia@Citadel.local>
2018-10-11 23:17:13 -0300

    Init

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <nuxt-link to="/">This is the header</nuxt-link>
+    <nuxt-link to="/about">This is the about</nuxt-link>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>This is the About</h1>
+</template>
